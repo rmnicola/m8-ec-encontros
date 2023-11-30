1afc20fdc412517b4528cdfc2081ccb9eb95c659
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2023-11-30 00:15:39 -0300

    Modifica exemplo stt whisper

--- exemplos/encontro10/stt_whisper.py
+++ exemplos/encontro10/stt_whisper.py
@@ -2,20 +2,20 @@ import argparse
 import whisper
 
 def main():
-    # Create the parser
-    parser = argparse.ArgumentParser(description="Transcribe audio files using Whisper.")
-
-    # Add the arguments
-    parser.add_argument('-m', '--model', type=str, required=True, help="The name of the Whisper model to use (e.g., 'small', 'medium', 'large').")
-    parser.add_argument('-f', '--file', type=str, required=True, help="The path to the MP3 file to transcribe.")
-    
-    # Parse the arguments
+    parser = argparse.ArgumentParser(
+            description="Transcreve áudio em texto usando o Whisper."
+            )
+    parser.add_argument(
+            '-m', '--model', type=str, required=True,
+            help="O nome do modelo a ser usado (e.g. small, medium)."
+            )
+    parser.add_argument(
+            '-f', '--file', type=str, required=True, 
+            help="Caminho para o arquivo de áudio"
+            )
     args = parser.parse_args()
 
-    # Load the model
     model = whisper.load_model(args.model)
-
-    # Transcribe the audio file
     result = model.transcribe(args.file)
     print(result["text"])
 
